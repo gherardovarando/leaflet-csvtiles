dc7227198cb2dbb2254014deabfdd4595c8c2eb9
gherardovarando <gherardo.varando@gmail.com>
2018-04-17 09:37:06 +0200

    return data of points

--- leaflet-csvtiles.js
+++ leaflet-csvtiles.js
@@ -256,7 +256,8 @@ if (L != undefined) {
         let point = {
           lat: this._origin[0] + scaleY * (results.data[0][this.options.columns.y] + reference.y),
           lng: this._origin[1] + scaleX * (results.data[0][this.options.columns.x] + reference.x),
-          level: results.data[0][this.options.columns.z]
+          level: results.data[0][this.options.columns.z],
+          data: results.data[0]
         }
         if (typeof cl === 'function') cl(point);
       }
